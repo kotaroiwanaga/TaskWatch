5a053b3fa2427d077332701aa86461a5f89974b4
kotaroiwanaga-ffs <38637016+kotaroiwanaga-ffs@users.noreply.github.com>
2019-09-25 21:49:40 +0900

    issue #1, #2 対応 ファイル名変更、ファイル分割など一部リファクタも含む

--- TaskWatch/Program.cs
+++ TaskWatch/Program.cs
@@ -16,7 +16,7 @@ namespace TaskWatch
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
